ecc29599f9bf13b18d8b1cc79af78186a36fb906
Thiago Lima <lietlaw364@gmail.com>
2022-06-26 14:41:35 -0300

    detail a review

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,10 +1,16 @@
 import AppCard from './shared/AppCard'
+import { useNavigate } from "react-router-dom";
 import { FaTimes, FaInfoCircle, FaEdit } from 'react-icons/fa'
 import { useContext } from 'react'
 import FeedbackContext from '../context/FeedbackContext'
 
 const FeedbackItem = ({ feedback }) => {
   const { deleteFeedback, editFeedback } = useContext(FeedbackContext)
+  const navigate = useNavigate()
+
+  const handleDetail = (id) => {
+    navigate(`/feedback/${id}`)
+  }
 
   return (
     <AppCard>
@@ -15,7 +21,7 @@ const FeedbackItem = ({ feedback }) => {
       <button onClick={() => editFeedback(feedback)} className="detail">
         <FaEdit size={20} color="purple" />
       </button>
-      <button className="edit">
+      <button onClick={() => handleDetail(feedback.id)} className="edit">
         <FaInfoCircle size={20} color="purple" />
       </button>
       <div className="text-display">{feedback.text}</div>
